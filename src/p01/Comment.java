178dd8495fb53ea122baf6da05931bf85401c02b
Jinah-engineer <jeeeengineer@gmail.com>
2021-03-25 10:28:21 +0900

    First test commit

---
+++ src/p01/Comment.java
@@ -0,0 +1,16 @@
+package p01;
+
+public class Comment {
+	public static void main(String[] args) {
+		System.out.println("주석"); // statement가 끝날 때마다 세미콜론을 꼭!
+		System.out.println("한 줄 주석");
+//		System.out.println("한 줄 주석");		
+		
+		// 한 줄 주석은 slash 2개
+		
+		System.out.println("여러 줄 주석");
+		/*
+		 *  별 여러 개 ,,, *****
+		 */
+	}
+}
